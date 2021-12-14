ebe2f3f7d3ba1556d2d9a6267c6bf5b5324b5930
dominic cyril bacasnot <92629705+tamweiwen@users.noreply.github.com>
2021-12-15 06:05:39 +0800

    chuchuchu final main branch with android architecture

--- app/src/main/java/com/infinityandroid/chatapp/activities/SignInViewModelFactory.java
+++ app/src/main/java/com/infinityandroid/chatapp/activities/SignInViewModelFactory.java
@@ -1,23 +1,8 @@
 package com.infinityandroid.chatapp.activities;
 
-import android.content.Context;
-
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
-
-import com.infinityandroid.chatapp.utilities.PreferenceManager;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Map;
-import javax.inject.Inject;
-import javax.inject.Provider;
-
-import kotlin.jvm.internal.Intrinsics;
 
 public final class SignInViewModelFactory implements ViewModelProvider.Factory {
     String email;
